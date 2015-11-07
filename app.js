d29cc02d6eb7642ccedc97d6d6e80327b079d82e
unknown <chougule.ds@gmail.com>
2015-11-07 16:59:37 +0530

    Union function added

--- app.js
+++ app.js
@@ -25,6 +25,26 @@ exports.difference = function(arr1, arr2) {
 	}
 	return result;
 }
+exports.union = function(arr1, arr2) {
+
+	var array = {};
+	if (!isArray(arr1) || !isArray(arr2)) {
+
+		throw new TypeError("Invalid argument, Please pass proper array argument");
+	}
+
+	var result = [];
+	for (var i = 0; i < arr1.length; i++) {
+		array[arr1[i]] = arr1[i];
+		result.push(arr1[i])
+	}
+
+	for (var i = 0; i < arr2.length; i++) {
+		if (array[arr2[i]] === undefined)
+			result.push(arr2[i])
+	}
+	return result;
+}
 
 /*exports.sum = function(arr) {
 	var result=0;

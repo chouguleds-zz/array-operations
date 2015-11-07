b175f67a78acce2035b132b46ebd1ca7adb2284c
unknown <chougule.ds@gmail.com>
2015-11-07 17:21:07 +0530

    Intersection function added

--- app.js
+++ app.js
@@ -46,6 +46,26 @@ exports.union = function(arr1, arr2) {
 	return result;
 }
 
+exports.intersection = function(arr1, arr2) {
+
+	var array = {};
+	if (!isArray(arr1) || !isArray(arr2)) {
+
+		throw new TypeError("Invalid argument, Please pass proper array argument");
+	}
+
+	var result = [];
+	for (var i = 0; i < arr1.length; i++) {
+		array[arr1[i]] = arr1[i];
+	}
+
+	for (var i = 0; i < arr2.length; i++) {
+		if (array[arr2[i]] !== undefined)
+			result.push(arr2[i])
+	}
+	return result;
+}
+
 /*exports.sum = function(arr) {
 	var result=0;
 	for(var i=0;i<arr.length;i++)

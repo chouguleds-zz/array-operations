fee470893c4f4da81de845c7f6faa08be6526ebe
unknown <dchougule@Tavwitpd032.internal.tavisca.com>
2015-11-19 18:29:50 +0530

    flatten function and tests added

--- app.js
+++ app.js
@@ -230,3 +230,42 @@ exports.areDistinct = function(arr1, arr2) {
 	return (!this.areEqual(arr1, arr2));
 
 }
+
+exports.flatten = function(arr, level) {
+
+	if (!isArray(arr)) {
+
+		throw new TypeError("Invalid argument, Please pass proper array argument");
+	}
+	var result = []
+	if (level === undefined)
+		return recursiveFlatten(arr, result);
+	else
+		return recursiveFlattenWithDepth(arr, result, level);
+}
+
+function recursiveFlatten(arr, result) {
+
+	for (var i = 0; i < arr.length; i++) {
+
+		if (isArray(arr[i])) {
+
+			recursiveFlatten(arr[i], result)
+		} else
+			result.push(arr[i])
+	}
+	return result;
+}
+
+function recursiveFlattenWithDepth(arr, result, level) {
+
+	for (var i = 0; i < arr.length; i++) {
+
+		if (isArray(arr[i]) && level > 0) {
+			level--;
+			recursiveFlattenWithDepth(arr[i], result, level)
+		} else
+			result.push(arr[i])
+	}
+	return result;
+}
